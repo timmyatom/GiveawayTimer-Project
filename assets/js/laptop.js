9f303649aa65e4faf10a6cf9868ed41b2717fa10
Olalere Timileyin Matthew <olalere432@gmail.com>
2024-02-27 21:20:44 +0100

    Finalized

--- assets/js/laptop.js
+++ assets/js/laptop.js
@@ -1,12 +1,23 @@
-const targetDate = new Date('2024-04-16T00:00:00').getTime();
-let timeRemaining = 10 * 24 * 60 * 60 * 1000; // 10 days in milliseconds
+let targetDate = localStorage.getItem('targetDate');
+let timeRemaining = parseInt(localStorage.getItem('timeRemaining')) || 10 * 24 * 60 * 60 * 1000; // This one is 10 days in milliseconds
+
+if (!targetDate) {
+    targetDate = Date.now(); // Set the target date to the current timestamp if it's not already set
+    localStorage.setItem('targetDate', targetDate);
+} else {
+    targetDate = parseInt(targetDate);
+}
 
 function updateCountdown() {
     timeRemaining -= 1000; // Subtract 1 second
 
     if (timeRemaining < 0) {
-        timeRemaining = 10 * 24 * 60 * 60 * 1000; // This one is to Reset to 10 days in milliseconds
+        timeRemaining = 10 * 24 * 60 * 60 * 1000; // Reset to 10 days in milliseconds
+        localStorage.setItem('timeRemaining', timeRemaining);
+    } else {
+        localStorage.setItem('timeRemaining', timeRemaining);
     }
+
     const finalDate = new Date(targetDate);
     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -19,6 +30,19 @@ function updateCountdown() {
     document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
 }
 
+function updateEndDate() {
+    const currentDate = Date.now();
+    const endDate = new Date(currentDate + timeRemaining);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const endDateString = endDate.toLocaleDateString('en-US', options);
+    document.getElementById('end-date').textContent = `The Giveaway ends on ${endDateString}`;
+}
+
+
 // Initial call and update every second
 updateCountdown();
-setInterval(updateCountdown, 1000);
+updateEndDate();
+setInterval(() => {
+    updateCountdown();
+    updateEndDate();
+}, 1000);
